f282d28a6601e6f06863e77506fd1e4cb4181a0c
XVC-THE-CODER <daffcode01@gmail.com>
2026-07-31 17:04:58 +0700

    Update filter.lua

--- setting/filter.lua
+++ setting/filter.lua
@@ -3,6 +3,7 @@ local PlaceScripts = {
   [90568084448279] = "https://raw.githubusercontent.com/XVC-THE-CODER/Renux-Hub/refs/heads/main/games/one-tap.lua",
   [142823291] = "https://raw.githubusercontent.com/XVC-THE-CODER/Renux-Hub/refs/heads/main/games/mm2.lua",
   [205224386] = "https://raw.githubusercontent.com/XVC-THE-CODER/Renux-Hub/refs/heads/main/games/hide-and-seek.lua",
+  [136801880565837] = "https://raw.githubusercontent.com/XVC-THE-CODER/Renux-Hub/refs/heads/main/games/flick.lua",
 }
 local FallbackLink = "https://pastebin.com/raw/LvRLf96s" local url = PlaceScripts[game.PlaceId] or FallbackLink
 loadstring(game:HttpGet(url))()
