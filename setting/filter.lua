efd3a4c9625f932e6fba87556c7d9bf95127ddb3
XVC-THE-CODER <daffcode01@gmail.com>
2026-08-03 20:41:55 +0700

    Update filter.lua

--- setting/filter.lua
+++ setting/filter.lua
@@ -1,7 +1,7 @@
 local PlaceScripts = {
     [537413528] = "https://raw.githubusercontent.com/XVC-THE-CODER/Renux-Hub/refs/heads/main/games/babft.lua",
     [90568084448279] = "https://raw.githubusercontent.com/XVC-THE-CODER/Renux-Hub/refs/heads/main/games/one-tap.lua",
-    [142823291] = "https://raw.githubusercontent.com/XVC-THE-CODER/Renux-Hub/refs/heads/main/games/mm2.lua",
+    [142823291] = "https://pastebin.com/raw/5WFZfhTh",
     [205224386] = "https://raw.githubusercontent.com/XVC-THE-CODER/Renux-Hub/refs/heads/main/games/hide-and-seek.lua",
     [136801880565837] = "https://raw.githubusercontent.com/XVC-THE-CODER/Renux-Hub/refs/heads/main/games/flick.lua",
 }
