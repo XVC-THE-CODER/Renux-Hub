c97b1cc638086b10610885c56b66d9472c1fa35e
SCRIPTHUB-dev-god <faruqaldaffa069@gmail.com>
2026-08-30 20:19:39 +0700

    Update script version from 1.2 to 1.3

--- games/mm2.lua
+++ games/mm2.lua
@@ -18,7 +18,7 @@ local window = library:CreateWindow({
 pcall(function()
     window:AddTag({title = "keyless", canclicked = false, callback = function() end})
     window:AddTag({title = "made in indonesia", canclicked = false, callback = function() end})
-    window:SetMovingText("script version 1.2")
+    window:SetMovingText("script version 1.3")
 end)
 local InfoTab = library:CreateTab("Information")
 local Tab = library:CreateTab("Main")
